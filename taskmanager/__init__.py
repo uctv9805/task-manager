251c620d425cdb12487dd1d29d5d2e94cda21646
Ash Marshall <ashley_marshall86@hotmail.co.uk>
2024-08-21 11:55:52 +0000

    fix indent issue

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 if os.environ.get("DEVELOPMENT") == "True":
      app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
- else:
+else:
      uri = os.environ.get("DATABASE_URL")
      if uri.startswith("postgres://"):
          uri = uri.replace("postgres://", "postgresql://", 1)
